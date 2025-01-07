35bb37f9de7ab4682512627b16353dcfecafe28f
Suffo soh raoul <sohraoulsuffo@gmail.com>
2025-01-07 04:26:16 +0100

    new year starts for me

--- feedback-app/feedback-app/src/App.js
+++ feedback-app/feedback-app/src/App.js
@@ -1,33 +1,13 @@
+import Header from "./components/header";
+
 function App() {
-  const title = "BLOG post";
-  const blog = "this is my blog post";
-  const comments = [
-    { id: 1, text: "Comment one" },
-    { id: 2, text: "Comment two" },
-    { id: 3, text: "Comment three " },
-  ];
-
-  const loading = false;
-  const showComments = true;
-const commentBlock = (
-  <div className="comments">
-    <h3>Comments ({comments.length})</h3>
-    <ul>
-      {comments.map((comment, index) => (
-        <li key={index}>{comment.text}</li>
-      ))}
-    </ul>
-  </div>
-);
-  if (loading) return <h1>Loading...</h1>;
   return (
-    <div className="container">
-      <h1> {title.toUpperCase()}</h1>
-      <p>{blog}</p>
-
-
-      {showComments &&  commentBlock }
-    </div>
+    <>
+      <Header text='FeedBack UI' />
+      <div className="container">
+        <h1>My App</h1>
+      </div>
+    </>
   );
 }
 export default App;
